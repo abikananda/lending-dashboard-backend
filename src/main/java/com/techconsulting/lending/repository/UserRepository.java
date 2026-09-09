869dfc0c5698a562763ae8f9bebdfd184f8b5a54
abikananda <abikananda@users.noreply.github.com>
2026-09-10 02:00:15 +0530

    Allow existing users to configure lender ID

--- src/main/java/com/techconsulting/lending/repository/UserRepository.java
+++ src/main/java/com/techconsulting/lending/repository/UserRepository.java
@@ -1,2 +1,2 @@
 package com.techconsulting.lending.repository; import com.techconsulting.lending.domain.User; import org.springframework.data.jpa.repository.JpaRepository; import java.util.Optional;
-public interface UserRepository extends JpaRepository<User,Long> { Optional<User> findByUsernameIgnoreCase(String username); boolean existsByUsernameIgnoreCaseOrEmailIgnoreCase(String username,String email); boolean existsByLenderIdIgnoreCase(String lenderId); }
+public interface UserRepository extends JpaRepository<User,Long> { Optional<User> findByUsernameIgnoreCase(String username); Optional<User> findByLenderIdIgnoreCase(String lenderId); boolean existsByUsernameIgnoreCaseOrEmailIgnoreCase(String username,String email); boolean existsByLenderIdIgnoreCase(String lenderId); }
