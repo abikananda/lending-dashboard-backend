315c38335081ad7c8f448df015b85c811d52aaf8
abikananda <abikananda@users.noreply.github.com>
2026-09-13 21:32:05 +0530

    Email before and after stats for manual uploads

---
+++ src/main/java/com/techconsulting/lending/config/AsyncConfiguration.java
@@ -0,0 +1,21 @@
+package com.techconsulting.lending.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfiguration {
+    @Bean(name = "uploadEmailExecutor")
+    Executor uploadEmailExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("upload-email-");
+        executor.initialize();
+        return executor;
+    }
+}
