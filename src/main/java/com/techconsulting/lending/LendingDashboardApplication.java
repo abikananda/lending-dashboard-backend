e03851ce8a9b0d298d4cbf91a291cbc6c5e4720d
abikananda <abikananda@users.noreply.github.com>
2026-09-13 16:54:20 +0530

    Reconcile repayment emails with bank credits

--- src/main/java/com/techconsulting/lending/LendingDashboardApplication.java
+++ src/main/java/com/techconsulting/lending/LendingDashboardApplication.java
@@ -3,8 +3,10 @@ package com.techconsulting.lending;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class LendingDashboardApplication {
   public static void main(String[] args) { SpringApplication.run(LendingDashboardApplication.class, args); }
